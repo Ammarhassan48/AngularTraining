039adfc068bb0e6ddab51e3958f7fee2825ecd7d
Ammar Shaukat <ammarhassan48@gmail.com>
2023-10-15 11:54:53 +0500

    added file , updated project config

---
+++ UI/codepulse/src/app/features/blog-post/models/add-blog-post.model.ts
@@ -0,0 +1,10 @@
+export interface AddBlogPost{
+    title: string;
+    shortDescripton: string;
+    content: string;
+    featuredImageUrl: string;
+    urlHandle: string;
+    author: string;
+    publishedDate: string;
+    isVisible: boolean;
+}
