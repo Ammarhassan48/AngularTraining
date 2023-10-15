de0db92b6337763109999ae4bfedb70e63a8a02c
Ammar Shaukat (Coeus) <coeus.ammar.shaukat@vwfspay.com>
2023-10-16 01:18:21 +0500

    Category Controller Create Method Unit Test class added.

---
+++ API/CodePulse.API/CodePulse.API.Tests/Controllers/CategoryController/CategoryControllerTestsBase.cs
@@ -0,0 +1,24 @@
+﻿using CodePulse.API.Controllers;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePulse.API.Tests.Controllers.CategoryController
+{
+    public class CategoryControllerTestsBase
+    {
+        //private readonly Mock<ICategoryRepository> categoryRepository;
+        //private readonly Mock<ILogger<CategoriesController>> logger;
+
+        public CategoryControllerTestsBase()
+        {
+            //categoryRepository = new Mock<ICategoryRepository>();
+            //logger = new Mock<ILogger<CategoriesController>>();
+        }
+    }
+}
